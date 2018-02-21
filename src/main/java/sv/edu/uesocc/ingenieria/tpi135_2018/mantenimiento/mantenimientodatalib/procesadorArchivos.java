2e3b5e0e2a2053213a42056a29cacdc15c51f542
Diana Magaña <mb15004@ues.edu.sv>
2018-02-20 22:13:20 -0600

    Trabajando con el método cargarArchivo y su prueba de unidad

---
+++ src/main/java/sv/edu/uesocc/ingenieria/tpi135_2018/mantenimiento/mantenimientodatalib/procesadorArchivos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.mantenimientodatalib;
+
+/**
+ *
+ * @author dmmaga
+ */
+public class procesadorArchivos {
+    
+    public boolean cargarArchivo(final String path){
+        return false;
+    }
+    
+}
---
+++ src/test/java/sv/edu/uesocc/ingenieria/tpi135_2018/mantenimiento/mantenimientodatalib/procesadorArchivosTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.mantenimientodatalib;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dmmaga
+ */
+public class procesadorArchivosTest {
+    
+    public procesadorArchivosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of cargarArchivo method, of class procesadorArchivos.
+     */
+    @org.junit.Test
+    public void testCargarArchivo() {
+        System.out.println("cargarArchivo");
+        String path = "";
+        procesadorArchivos instance = new procesadorArchivos();
+        boolean expResult = false;
+        boolean result = instance.cargarArchivo(path);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("Esto va a fallar");
+    }
+    
+}
