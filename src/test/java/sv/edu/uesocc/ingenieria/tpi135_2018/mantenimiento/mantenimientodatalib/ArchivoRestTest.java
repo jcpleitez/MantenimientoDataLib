c2f6ee4c2108e5c5a92ffb1674b49f9e00c0f402
Juan Pleitez <juanpc13lolol@gmail.com>
2018-03-10 14:31:14 -0600

    Falta hacer el mock de rest para la prueba de unidad

---
+++ src/test/java/sv/edu/uesocc/ingenieria/tpi135_2018/mantenimiento/mantenimientodatalib/ArchivoRestTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.tpi135_2018.mantenimiento.mantenimientodatalib;
+
+import java.net.URI;
+import java.util.List;
+import javax.ws.rs.client.ClientBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+/**
+ *
+ * @author jcpleitez
+ */
+public class ArchivoRestTest {
+    
+    public ArchivoRestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of postLista method, of class ArchivoRest.
+     */
+    
+    //@RunWith(PowerMockRunner.class)
+    //@PrepareForTest(ClientBuilder.class)
+    
+    @Test
+    public void testPostLista() {
+        System.out.println("*Prueba postLista");
+        List<List<String>> lista = null;
+        ArchivoRest instance = new ArchivoRest();
+        URI expResult = null;
+        URI result = instance.postLista(lista);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
